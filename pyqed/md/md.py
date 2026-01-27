970b59845d9365dc32e0a9d5cfe6fee62d5ac61b
Bing Gu <binggu56@gmail.com>
2026-01-27 15:55:26 +0800

    add spin boson model

---
+++ pyqed/md/md.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 26 08:50:57 2026
+
+BOMD
+
+@author: bingg
+"""
+
+class MDBase:
+    def __init__(self):
+        pass
+
+
+
+class NVE(MDBase):
+    def run(self):
+        pass
--- pyqed/nrg/nrg.py
+++ pyqed/nrg/nrg.py
@@ -69,49 +69,6 @@ class Boson(Cavity):
 #     return s0, sx, sy, sz
 
 
-class SBM:
-    """
-    spin-boson model
-    """
-    def __init__(self, epsilon, Delta, omegac=1):
-        """
-
-
-        Parameters
-        ----------
-        epsilon : TYPE
-            DESCRIPTION.
-        Delta : TYPE
-            DESCRIPTION.
-        omegac : TYPE, optional
-            cutoff frequency. The default is 1.
-
-        Returns
-        -------
-        None.
-
-        """
-
-        self.omegac = omegac
-
-        I, X, Y, Z = pauli()
-
-        self.H = 0.5 * (- epsilon * Z + X * Delta)
-
-    def spectral_density(self, s=1, alpha=1):
-        pass
-
-    def discretize(self):
-        pass
-
-    def to_wilson_chain(self):
-        pass
-
-    def HEOM(self):
-        pass
-
-    def Redfield(self):
-        pass
 
 
 
